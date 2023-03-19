7eaa8a1737125d4d921f00505066bcb30be9f9da
Sparkmod <rafadagher@gmail.com>
2023-03-19 12:22:23 -0400

    Feat - Models and Repository
    - Added models and their repositories and interfaces.
    - Added the Storage that will work as a mock.

---
+++ src/Domain/Interfaces/IContactRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Interfaces;
+
+public interface IContactRepository
+{
+    public Contact Create(Contact contact);
+    public bool Update(Contact updatedContact);
+    public bool DeleteById(int id);
+    public Contact GetById(int id);
+    public IReadOnlyCollection<Contact> GetAll();
+}
---
+++ src/Infrastructure/Storages/ContactStorage.cs
@@ -0,0 +1,13 @@
+﻿namespace Infrastructure.Storages;
+
+public class ContactStorage
+{
+    public List<Contact> Contacts { get; } = new ()
+    {
+        new Contact { Id = 1, FirstName = "John", LastName = "Doe", Email = "john.doe@example.com", PhoneNumber = "555-123-4567" },
+        new Contact { Id = 2, FirstName = "Jane", LastName = "Doe", Email = "jane.doe@example.com", PhoneNumber = "555-987-6543" },
+        new Contact { Id = 3, FirstName = "Alice", LastName = "Smith", Email = "alice.smith@example.com", PhoneNumber = "555-567-8910" },
+        new Contact { Id = 4, FirstName = "Bob", LastName = "Johnson", Email = "bob.johnson@example.com", PhoneNumber = "555-246-8013" },
+        new Contact { Id = 5, FirstName = "Charlie", LastName = "Brown", Email = "charlie.brown@example.com", PhoneNumber = "555-369-1472" }
+    };
+}
